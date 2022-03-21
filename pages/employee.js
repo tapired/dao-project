7618616bb81fdae958562dbe304e953bf3e7c01c
bulutarda <arda.bulut@ozu.edu.tr>
2022-03-22 02:07:33 +0300

    done enableWeb3

--- pages/employee.js
+++ pages/employee.js
@@ -3,43 +3,39 @@ import { ethers } from "ethers";
 import Web3Modal from "web3modal";
 import { useState, useEffect } from "react";
 import { daoaddress } from "../config";
-import {useWeb3ExecuteFunction,useMoralis} from 'react-moralis'
+import { useWeb3ExecuteFunction, useMoralis } from "react-moralis";
+
 
 import Dao from "../artifacts/contracts/DAO.sol/DAO.json";
 
-
-
-
 const employee = () => {
-  const { account, user } = useMoralis();
+  const { account, enableWeb3, isWeb3Enabled, isAuthenticated } = useMoralis();
   const [url, setUrl] = useState("");
   const [companyid, setCompanyId] = useState("");
   const [adress, setAdress] = useState("");
-  const [owner, setOwner] = useState(false)
-
-  
- useEffect(()=>{
+  const [owner, setOwner] = useState(false);
+  useEffect(()=>{
     isOwner()
-     
- },[])
-  const isOwner = async () => {
-      const web3Modal = new Web3Modal();
-      const connection = await web3Modal.connect();
-      const provider = new ethers.providers.Web3Provider(connection);
-      const signer = provider.getSigner();
-      let contract = new ethers.Contract(daoaddress, Dao.abi, provider);
-      let x = await contract.companyDescription(1);
-      const companyOwner = x.owner 
-      //const account = await user.get("account")[0]
-      //console.log(account)
-      if(companyOwner == account) {
-        setOwner(true)
-        
-      }
-  }
- 
-  
-  
+  },[account])
+  useEffect(()=>{     
+    enableWeb3()    
+   },[isWeb3Enabled,])
+   
+   const newAccount = {
+     name: account
+   }
+   console.log(newAccount)
+   const isOwner = async () => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    let contract = new ethers.Contract(daoaddress, Dao.abi, provider);
+    let x = await contract.companyDescription(1);
+    const companyOwner = x.owner 
+    
+}
+
   async function signCompany() {
     if (url.length > 1) {
       const web3Modal = new Web3Modal();
@@ -49,7 +45,7 @@ const employee = () => {
 
       let contract = new ethers.Contract(daoaddress, Dao.abi, signer);
       let company = await contract.companyDescription(2);
-      
+
       let transaction = contract.signCompany(url);
     } else {
       alert("!!!!");
@@ -63,16 +59,15 @@ const employee = () => {
     let contract = new ethers.Contract(daoaddress, Dao.abi, signer);
     let company = await contract.signEmployee(companyid, adress);
   }
-  async function signMurat(){
+  async function signMurat() {
     const web3Modal = new Web3Modal();
     const connection = await web3Modal.connect();
     const provider = new ethers.providers.Web3Provider(connection);
     const signer = provider.getSigner();
     let contract = new ethers.Contract(daoaddress, Dao.abi, signer);
-    let x = await contract.companyDescription(1)
-    
+    let x = await contract.companyDescription(1);
   }
-  
+  signMurat();
 
   return (
     <div className="flex flex-row justify-center items-center gap-x-5 mt-20">
